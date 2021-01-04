917972413777a9a93e728909027f760d41f05b9a
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-04 11:11:38 -0800

    New files were added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a new React app</h1>
+        <p>This is really working!</p>
+        
+        <Person />
+        <Person />
+        <Person />
+        <Person />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>I'm a person from Person.js and I'm {Math.ceil(Math.random()*100)} years old!</p>
+};
+
+export default person
