061907f8e96106743e493a1be82bbe1adfab2ad6
Iurii Lopatenko <yalopatenko@gmail.com>
2021-01-04 12:37:57 -0800

    Lesson 3.14 - Understanding and using STATE (with class-based React components)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Karl1', age: 25},
+      {name: 'John1', age: 19},
+      {name: 'Kate1', age: 35},
+      {name: 'Jane1', age: 70},
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a new React app</h1>
         <p>This is really working!</p>
-        <Person name='Karl' age='24'/>
-        <Person name='John' age='19'/>
-        <Person name='Kate' age='35'> My hoobies: Racing </Person>
-        <Person name='Jane' age='70'/>
+        <button>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My hoobies: Racing </Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
       </div>
     );
   }
